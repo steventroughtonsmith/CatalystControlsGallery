1f73721624de94522b14de37bb6b496381a44d63
Steven Troughton-Smith <steventroughtonsmith@gmail.com>
2021-08-18 00:35:14 +0100

    Added color picker.

--- App/ContentView.swift
+++ App/ContentView.swift
@@ -28,7 +28,7 @@ struct ContentView: View {
 					HISlidersView()
 					HIProgressIndicatorView()
 					HISegmentedControlView()
-					
+					HIColorsView()
 				}
 				.padding()
 			}
---
+++ App/Pages/HIColorsView.swift
@@ -0,0 +1,52 @@
+//
+//  HIColorsView.swift
+//  HI Toolbox
+//
+//  Created by Steven Troughton-Smith on 17/08/2021.
+//
+
+import SwiftUI
+import UIKit
+
+struct HIColorWell: View, UIViewRepresentable {
+	typealias UIViewType = UIColorWell
+	let view = UIColorWell()
+	var action:(_ color:UIColor?) -> Void = { color in }
+	
+	func makeUIView(context: Context) -> UIViewType {
+		view.selectedColor = .red
+		return view
+	}
+	
+	func updateUIView(_ uiView: UIViewType, context: Context) {
+		
+		view.addAction(UIAction(handler: { _ in
+			self.action(view.selectedColor)
+		}), for: .valueChanged)
+	}
+}
+
+
+struct HIColorsView: View {
+	@State var a = Color.red
+	
+	var body: some View {
+		VStack(alignment:.leading) {
+			Text("Color")
+				.bold()
+			
+			HStack {
+				HIColorWell(action: { color in
+					guard let color = color else { return }
+					
+					a = Color(color)
+				})
+				.frame(width: UIFloat(40), height: UIFloat(40))
+				
+				Text("Colored label")
+					.foregroundColor(a)
+			}
+		}
+		.padding()
+	}
+}
