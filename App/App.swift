b692dc8f88ec8d092e6e75eedbb41fb41c8341b4
Jed Fox <git@jedfox.com>
2021-08-18 13:33:28 -0400

    Fix black background on iOS

--- App/App.swift
+++ App/App.swift
@@ -15,9 +15,9 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 	func applicationDidFinishLaunching(_ application: UIApplication) {
 		window = UIWindow()
 		window?.rootViewController = UIHostingController(rootView: ContentView())
-		window?.rootViewController?.view.backgroundColor = .clear
-		
+
 		#if targetEnvironment(macCatalyst)
+		window?.rootViewController?.view.backgroundColor = .clear
 		window?.windowScene?.titlebar?.toolbar = NSToolbar()
 		#endif
 		
